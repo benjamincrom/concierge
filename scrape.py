82597358cd4b024b406c385e562cdea067f8e461
bcrom <bcrom@23andme.com>
2013-09-19 14:51:17 -0700

    added RogerEbert files

---
+++ scrape.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+class GetMediaMetadata:
+    """This class will pull down data and metadata for reviews and media and store it in the database"""
+
+    def get_imdb_data(self, title):
