2927d0841a62894aec1b5552ce49c4cfa0df3f71
bcrom <bcrom@23andme.com>
2013-09-19 16:06:35 -0700

    added to scrape class

--- scrape.py
+++ scrape.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 
+import urllib
+import json
+
 class GetMediaMetadata:
     """This class will pull down data and metadata for reviews and media and store it in the database"""
+    def __init__(self, title):
+        self.title = title
+        self.imdb_json_str = self.get_imdb_json_str()
+        self.imdb_json_dict = self.encode_json_str_into_dict()
 
-    def get_imdb_data(self, title):
+    def get_imdb_json_str(self):
+        quoted_title = urllib.quote(self.title)
+        json_response = urllib.urlopen("http://www.omdbapi.com/?t=%s" %(quoted_title))
+        return json_response.read()
+
+    def encode_json_str_into_dict(self):
+        return json.loads(self.imdb_json_str)
+
