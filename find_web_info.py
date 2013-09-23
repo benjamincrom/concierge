c4eee3128b3076fff00d6678f0ab00fd7be2d004
bcrom <bcrom@23andme.com>
2013-09-23 16:39:34 -0700

    additional upgrades to metadata fetch script

--- find_web_info.py
+++ find_web_info.py
@@ -2,19 +2,55 @@
 
 import urllib
 import json
+import re
 
-class GetMediaMetadata:
-    """This class will pull down data and metadata for reviews and media and store it in the database"""
+class Video:
+    """This class will pull metadata from remote sources and store it in a video object"""
     def __init__(self, title):
-        self.title = title
-        self.imdb_json_str = self.get_imdb_json_str()
-        self.imdb_json_dict = self.encode_json_str_into_dict()
+        # Pull down JSON from IMDB keyed on title
+        imdb_json_str = self.get_imdb_json_str(title)
+        imdb_json_dict = self.encode_json_str_into_dict(imdb_json_str)
 
-    def get_imdb_json_str(self):
-        quoted_title = urllib.quote(self.title)
+        # Store IMDB metadata in video object
+        self.title = imdb_json_dict['Title']
+        self.rating = imdb_json_dict['Rated']
+        self.plot = imdb_json_dict['Plot']
+        self.poster_url = imdb_json_dict['Poster']
+        self.year = imdb_json_dict['Year']
+        self.video_type = imdb_json_dict['Type']
+
+        # Split comma separated fields into python lists
+        self.writer_list = [str(writer_name.strip()) for writer_name in imdb_json_dict['Writer'].split(',')]
+        self.director_list = [str(director_name.strip()) for director_name in imdb_json_dict['Director'].split(',')]
+        self.actor_list = [str(actor_name.strip()) for actor_name in imdb_json_dict['Actors'].split(',')]
+        self.genre_list = [str(genre_name.strip()) for genre_name in imdb_json_dict['Genre'].split(',')]
+
+        # Calculate runtime in minutes and store in object
+        runtime = imdb_json_dict['Runtime']
+        m = re.match("(\d+) h (\d+) min", runtime) 
+        hours, minutes = m.groups()
+        self.length = int(hours)*60 + int(minutes)
+
+    @classmethod
+    def get_imdb_json_str(cls, title):
+        quoted_title = urllib.quote(title)
         json_response = urllib.urlopen("http://www.omdbapi.com/?t=%s" %(quoted_title))
         return json_response.read()
 
-    def encode_json_str_into_dict(self):
-        return json.loads(self.imdb_json_str)
+    @classmethod
+    def encode_json_str_into_dict(cls, json_str):
+        return json.loads(json_str)
 
+if __name__ == '__main__':
+    g = Video('The Terminal')
+    print g.title
+    print g.rating
+    print g.plot
+    print g.poster_url
+    print g.writer_list
+    print g.director_list
+    print g.actor_list
+    print g.genre_list
+    print g.year
+    print g.length
+    print g.video_type
