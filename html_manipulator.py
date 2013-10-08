4d731b795c6dad491b9ef87afe185dc25265b90c
Benjamin Crom <benjamincrom@gmail.com>
2013-10-07 22:50:17 -0700

    reorg done

---
+++ html_manipulator.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+
+import re
+import urllib2
+
+from HTMLParser import HTMLParser
+
+
+GOOGLE_QUERY_URL = "http://www.google.com/search?q=%s"
+SPOOFED_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept': '``text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
+RETRIEVE_HTML_ERROR = "ERROR: URL '%s' is not a valid page"
+REGEX_NOT_FOUND_ERROR = "ERROR: Target regex '%s' not found--this value cannot be null"
+
+GOOGLE_REGEX = re.compile("<h2 class=\"hd\">Search Results.*?<a href=\"(.+?)\"", re.DOTALL)
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+def get_top_google_result_url(search_string):
+    formatted_search_string = search_string.replace(' ', '+')
+    html = retrieve_html_from_url(GOOGLE_QUERY_URL % formatted_search_string)
+    return use_regex(GOOGLE_REGEX, html, False)
+
+
+def retrieve_html_from_url(url):
+    try:
+        req = urllib2.Request(url, headers=SPOOFED_HEADERS)
+        html = urllib2.urlopen(req).read().decode('utf-8').encode('ascii','ignore')
+    except AttributeError:
+        print RETRIEVE_HTML_ERROR % url
+        raise
+    return html
+
+
+def remove_html_tags(str):
+    if str:
+        s = MLStripper()
+        s.feed(str)
+        return_str = s.get_data()
+    else:
+        return_str = None
+    return return_str
+
+
+def use_regex(given_regex, target_str, can_be_null):
+    match = given_regex.search(target_str)
+    try:
+        return_str = str(match.groups()[0]).strip()
+    except (AttributeError, IndexError):
+        return_str = None
+        if not can_be_null:
+            print REGEX_NOT_FOUND_ERROR % given_regex
+            raise
+    return return_str
---
+++ roger_ebert_scraper.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+
+import html_manipulator
+import re
+
+from datetime import datetime
+
+
+LOCAL_LINK_PREFIX = '<a href="'
+EBERT_LINK_PREFIX = '<a href="http://www.rogerebert.com'
+EBERT_FULL_STAR = 'icon-star-full'
+EBERT_GOOGLE_QUERY_STRING = "rogerebert.com %s"
+EBERT_HALF_STAR = 'icon-star-half'
+EBERT_REVIEW_NOT_FOUND = "There is no review on rogerebert.com for this title: %s"
+EBERT_REVIEW_URL = "http://www.rogerebert.com/reviews/%s"
+EBERT_SITE_TITLE = "RogerEbert.com"
+EBERT_URL_DELIMITER = '-'
+
+EBERT_REVIEW_REGEX = re.compile('<div itemprop="reviewBody">(.+?)</div>', re.DOTALL)
+EBERT_AUTHOR_REGEX = re.compile('<meta content="(.+?)" name="author">')
+EBERT_DATE_REGEX = re.compile('itemprop="datePublished">(.+?)</time>')
+EBERT_STARS_REGEX = re.compile('itemprop="reviewRating"(.+?)</span>', re.DOTALL)
+
+
+def scrape_rogerebert_data(title):
+    return_dict = None
+    ebert_review_url = html_manipulator.get_top_google_result_url(EBERT_GOOGLE_QUERY_STRING % title)
+    ebert_review_html = html_manipulator.retrieve_html_from_url(ebert_review_url)
+    review_text_match = EBERT_REVIEW_REGEX.search(ebert_review_html)
+    if review_text_match:
+        review_text = review_text_match.groups()[0]
+        formatted_review_text = _format_ebert_review_text(review_text)
+
+        review_author_match = EBERT_AUTHOR_REGEX.search(ebert_review_html)
+        review_author = review_author_match.groups()[0]
+
+        review_date_match = EBERT_DATE_REGEX.search(ebert_review_html)
+        review_date_string = review_date_match.groups()[0]
+        review_datetime = datetime.strptime(review_date_string, '%B %d, %Y')
+        review_date = review_datetime.date()
+
+        review_stars_match = EBERT_STARS_REGEX.search(ebert_review_html)
+        review_stars_string = review_stars_match.groups()[0]
+        review_percent_score = _compute_ebert_percent_score(review_stars_string)
+
+        return_dict = {}
+        return_dict["formatted_review_text"] = formatted_review_text
+        return_dict["review_author"] = review_author
+        return_dict["review_source"] = EBERT_SITE_TITLE
+        return_dict["review_date"] = review_date.strftime("%Y-%m-%d")
+        return_dict["review_percent_score"] = review_percent_score
+
+    return return_dict
+
+
+def _compute_ebert_percent_score(review_stars_string):
+    full_stars = len(re.findall(EBERT_FULL_STAR, review_stars_string))
+    half_stars = len(re.findall(EBERT_HALF_STAR, review_stars_string))
+    review_percent_score = (full_stars*2 + half_stars)/8.0
+    return review_percent_score
+
+
+def _format_ebert_review_text(review_text):
+    review_text = review_text.replace('\n', '')
+    formatted_review_text = review_text.replace(LOCAL_LINK_PREFIX, EBERT_LINK_PREFIX)
+    return formatted_review_text
