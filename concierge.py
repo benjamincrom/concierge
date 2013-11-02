9ac715f3be0caefc18c2c8008b32310cb8033b65
bcrom <bcrom@23andme.com>
2013-11-01 19:04:31 -0700

    Finished finally

--- concierge.py
+++ concierge.py
@@ -63,7 +63,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('<html><head></head><body>')
 
         video_query = Video.all()
-        for q in video_query.run(limit=50):
+        for q in video_query.run(limit=500):
             genre_str = ''
             for genre in q.genre_list:
                 if genre_str:
@@ -73,43 +73,52 @@ class MainPage(webapp2.RequestHandler):
 
             occupation_dict = {}
             for name_occupation_key in q.name_occupation_key_list:
-                occupation_obj = NameOccupation.all().filter('__key__ >', name_occupation_key).get()
+                occupation_obj = NameOccupation.get(name_occupation_key)
                 if occupation_obj.occupation not in occupation_dict:
                     occupation_dict[str(occupation_obj.occupation)] = []
 
                 occupation_dict[str(occupation_obj.occupation)].append(str(occupation_obj.name))
+
+            review_obj_list = Review.all().ancestor(q)
 
             self.response.write('<table>')
             self.response.write('<tr><td></td><td><img src="%s"></td></tr>' % q.poster_url)
             self.response.write('<tr><td>Title: </td><td>%s</td></tr>' % q.title)
             self.response.write('<tr><td>Year: </td><td>%s</td></tr>' % q.year)
             self.response.write('<tr><td>Plot: </td><td>%s</td></tr>' % q.plot)
-            self.response.write('<tr><td>Tagline: </td><td>%s</td></tr>' % q.tagline)
-            self.response.write('<tr><td>Budget: </td><td>%s</td></tr>' % q.budget)
-            self.response.write('<tr><td>Gross: </td><td>%s</td></tr>' % q.gross)
+            self.response.write('<tr><td>Tagline: </td><td>%s</td></tr>' % q.tagline) 
+            if q.budget:
+                self.response.write('<tr><td>Budget: </td><td>%s</td></tr>' % q.budget)
+
+            if q.gross:
+                self.response.write('<tr><td>Gross: </td><td>%s</td></tr>' % q.gross)
+
             self.response.write('<tr><td>Rating: </td><td>%s</td></tr>' % q.rating)
             self.response.write('<tr><td>Video Type: </td><td>%s</td></tr>' % q.video_type)
             self.response.write('<tr><td>Genres: </td><td>%s</td></tr>' % genre_str)
             self.response.write('<tr><td>Aspect Ratio: </td><td>%s</td></tr>' % q.aspect_ratio)
-            self.response.write('<tr><td>Score: </td><td>%s</td></tr>' % q.score)
+            self.response.write('<tr><td>Score: </td><td><strong>%s</strong></td></tr>' % q.score)
             self.response.write('<tr><td>Length: </td><td>%s</td></tr>' % q.length)
             self.response.write('<tr><td>IMDB ID: </td><td>%s</td></tr>' % q.imdb_id)
 
-            self.response.write('<tr><td></td><td></td></tr>')
-
-            self.response.write('<tr><td>occupation list: </td><td><table>')
             for (occupation, person_list) in occupation_dict.iteritems():
                 self.response.write('<tr><td><br /></td><td></td></tr>')
-                self.response.write('<tr><td><strong>' + occupation + '(s):</strong></td><td>')
+                self.response.write('<tr><td>' + occupation + '(s):</td><td>')
                 for person in person_list:
                     self.response.write(person + '<br />')
 
-                self.response.write('</td></tr>')
+            for review_obj in review_obj_list:
+                self.response.write('<tr><td><br /></td><td></td></tr>')
+                self.response.write('<tr><td>%s: </td><td><strong>%s</strong></td></tr>' % (review_obj.review_source, 
+                                                                                          review_obj.review_score))
+                if review_obj.review_author:
+                    self.response.write('<tr><td></td><td>%s</td></tr>' % review_obj.review_author)
+                if review_obj.review_content:
+                    self.response.write('<tr><td></td><td>%s</td></tr>' % review_obj.review_content)
+                if review_obj.review_date:
+                    self.response.write('<tr><td></td><td>Review Date: %s</td></tr>' % review_obj.review_date)
 
-            self.response.write('</table><br /></td></tr>')
-
-            self.response.write('<tr><td>review list: </td><td>%s</td></tr>' % q.review_key_list)
-            self.response.write('</table><br /><br />')
+            self.response.write('</table><br /><br /><br /><br /><br />')
 
         self.response.write('</body></html>')
 
--- ingest_into_datastore.py
+++ ingest_into_datastore.py
@@ -7,6 +7,7 @@ import webapp2
 
 from google.appengine.ext import db
 
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
@@ -15,6 +16,12 @@ class MainPage(webapp2.RequestHandler):
         json_str_list = open('json_titles.txt', 'r').readlines()
         json_obj_list = [json.loads(this_str) for this_str in json_str_list]
         for dict_obj in json_obj_list:
+            # 'v' is our main video object for each title
+            # All reviews are children of v
+            # All collaborators are NameOccupation foreign keys in a list
+            video_key_name = str(dict_obj['title']) + '-' + str(dict_obj['year'])
+            v = Video(key_name=video_key_name)
+
             # Print IMDB data
             director_role_list = [(director, "Director") for director in dict_obj["director_list"]]
             writer_role_list = [(writer, "Writer") for writer in dict_obj["writer_list"]]
@@ -36,18 +43,15 @@ class MainPage(webapp2.RequestHandler):
                 dict_obj["score"] = None
 
             for (person, role) in person_role_list:
-                # Check if entry is already in database and get key if it is
-                query = NameOccupation.all().filter('name =', person).filter('occupation =', role)
-                key = query.get(keys_only=True)
-                if not key:
-                    n = NameOccupation(name=person, occupation=role)
-                    key = n.put()
-                person_role_key_list.append(key)
+                person_role_key = str(person) + '-' + str(role)
+                n = NameOccupation.get_or_insert(person_role_key, name=person, occupation=role)
+                person_role_key_list.append(n.key())
 
             # Ebert Review
             if 'formatted_review_text' in dict_obj:
                 ebert_review_date = datetime.datetime.strptime(str(dict_obj['review_date']), "%Y-%m-%d").date()
                 r = Review(
+                    parent=v,
                     review_score=dict_obj['review_percent_score'],
                     review_author=dict_obj['review_author'],
                     review_source=dict_obj['review_source'],
@@ -55,73 +59,74 @@ class MainPage(webapp2.RequestHandler):
                     review_date=ebert_review_date,
                 )
                 ebert_review_key = r.put()
-                review_key_list.append(ebert_review_key)
 
             # Metacritic Review
             if 'metacritic_metascore_meter' in dict_obj:
                 r = Review(
+                    parent=v,
                     review_score=dict_obj['metacritic_metascore_meter'],
                     review_content=str(dict_obj['metacritic_metascore_total']),
                     review_source='Metacritic Metascore',
                 )
                 metacritic_metascore_key = r.put()
-                review_key_list.append(metacritic_metascore_key)
 
             if 'metacritic_userscore_meter' in dict_obj:
                 r = Review(
+                    parent=v,
                     review_score=dict_obj['metacritic_userscore_meter'],
                     review_content=str(dict_obj['metacritic_userscore_total']),
                     review_source='Metacritic Userscore',
                 )
                 metacritic_userscore_key = r.put()
-                review_key_list.append(metacritic_userscore_key)
 
             # Rottentomatoes Review
             if 'audience_meter' in dict_obj:
                 r = Review(
+                    parent=v,
                     review_score=dict_obj['audience_meter'],
                     review_content="%s (%s)" % (dict_obj['audience_avg_score'], dict_obj['audience_total']),
                     review_source='Rottentomatoes Audience Meter',
                 )
                 rottentomatoes_audience_key = r.put()
-                review_key_list.append(rottentomatoes_audience_key)
 
             if 'top_critics_rotten' in dict_obj:
                 r = Review(
+                    parent=v,
                     review_score=dict_obj['top_critics_meter'],
-                    review_content="%s (+%s, -%s)" % (dict_obj['top_critics_avg_score'], dict_obj['top_critics_fresh'], dict_obj['top_critics_rotten']),
+                    review_content="%s (+%s, -%s)" % (dict_obj['top_critics_avg_score'], 
+                                                      dict_obj['top_critics_fresh'], 
+                                                      dict_obj['top_critics_rotten']),
                     review_source='Rottentomatoes Top Critics',
                 )
                 rottentomatoes_top_critics_key = r.put()
-                review_key_list.append(rottentomatoes_top_critics_key)
 
             if 'all_critics_rotten' in dict_obj:
                 r = Review(
+                    parent=v,
                     review_score=dict_obj['all_critics_meter'],
-                    review_content="%s (+%s, -%s)" % (dict_obj['all_critics_avg_score'], dict_obj['all_critics_fresh'], dict_obj['all_critics_rotten']),
+                    review_content="%s (+%s, -%s)" % (dict_obj['all_critics_avg_score'], 
+                                                      dict_obj['all_critics_fresh'], 
+                                                      dict_obj['all_critics_rotten']),
                     review_source='Rottentomatoes All Critics',
                 )
                 rottentomatoes_all_critics_key = r.put()
-                review_key_list.append(rottentomatoes_all_critics_key)
 
-            v = Video(
-                score=dict_obj["score"],
-                aspect_ratio=dict_obj["aspect_ratio"],
-                year=dict_obj["year"],
-                length=dict_obj["length"],
-                title=dict_obj["title"],
-                rating=dict_obj["rating"],
-                imdb_id=dict_obj["imdb_id"],
-                poster_url=dict_obj["imdb_poster_url"],
-                plot=dict_obj["plot"],
-                tagline=dict_obj["tagline"],
-                gross=dict_obj["gross"],
-                budget=dict_obj["budget"],
-                video_type=dict_obj["video_type"],
-                genre_list=dict_obj["genre_list"],
-                name_occupation_key_list=person_role_key_list,
-                review_key_list=review_key_list,
-            )
+            v.score=dict_obj["score"]
+            v.aspect_ratio=dict_obj["aspect_ratio"]
+            v.year=dict_obj["year"]
+            v.length=dict_obj["length"]
+            v.title=dict_obj["title"]
+            v.rating=dict_obj["rating"]
+            v.imdb_id=dict_obj["imdb_id"]
+            v.poster_url=dict_obj["imdb_poster_url"]
+            v.plot=dict_obj["plot"]
+            v.tagline=dict_obj["tagline"]
+            v.gross=dict_obj["gross"]
+            v.budget=dict_obj["budget"]
+            v.video_type=dict_obj["video_type"]
+            v.genre_list=dict_obj["genre_list"]
+            v.name_occupation_key_list=person_role_key_list
+
             v.put()
 
 
