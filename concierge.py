b11584ca269c81cabfc9c974d7f2844d9f60bd82
bcrom <bcrom@23andme.com>
2013-11-04 16:51:42 -0800

    API

--- concierge.py
+++ concierge.py
@@ -1,127 +1,53 @@
 
 
-from google.appengine.ext import db
-
+import json
 import webapp2
 
-
-class NameOccupation(db.Model):
-    name = db.StringProperty()
-    occupation = db.StringProperty()
-
-
-class Review(db.Model):
-    review_score = db.FloatProperty()
-    review_author = db.StringProperty()
-    review_source = db.StringProperty()
-    review_content = db.TextProperty()
-    review_date = db.DateProperty()
-
-
-class Series(db.Model):
-    series_name = db.StringProperty()
-    total_episodes_in_series = db.IntegerProperty()
-    total_seasons_in_series = db.IntegerProperty()
-    genre_list = db.StringListProperty()
-    creator = db.ReferenceProperty(NameOccupation)
-    season_key_list = db.ListProperty(db.Key)
-
-
-class Season(db.Model):
-    season_number = db.IntegerProperty()
-    total_episodes_in_season = db.IntegerProperty()
-    series = db.ReferenceProperty(Series)
-    review_key_list = db.ListProperty(db.Key)
-
-
-class Video(db.Model):
-    title = db.StringProperty()
-    poster_url = db.StringProperty()
-    plot = db.TextProperty()
-    tagline = db.StringProperty()
-    gross = db.TextProperty()
-    imdb_id = db.StringProperty()
-    budget = db.TextProperty()
-    video_type = db.StringProperty()
-    rating = db.StringProperty()
-    genre_list = db.StringListProperty()
-    aspect_ratio = db.FloatProperty()
-    score = db.FloatProperty()
-    episode_number_in_season = db.IntegerProperty()
-    episode_number_in_total = db.IntegerProperty()
-    year = db.IntegerProperty()
-    length = db.IntegerProperty()
-    name_occupation_key_list = db.ListProperty(db.Key)
-    review_key_list = db.ListProperty(db.Key)
-    season = db.ReferenceProperty(Season)
+import models
 
 
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<html><head></head><body>')
+        self.response.headers['Content-Type'] = 'application/json'
 
-        video_query = Video.all()
-        for q in video_query.run(limit=500):
-            genre_str = ''
-            for genre in q.genre_list:
-                if genre_str:
-                    genre_str += ', '
+        video_query = models.Video.all()
+        for q in video_query.run(limit=50):
+            # Add video data to return dictionary
+            return_dict = {'poster_url': q.poster_url,
+                           'title': q.title,
+                           'year': q.year,
+                           'plot': q.plot,
+                           'tagline': q.tagline,
+                           'budget': q.budget,
+                           'gross': q.gross,
+                           'rating': q.rating,
+                           'video_type': q.video_type,
+                           'genre_list': q.genre_list,
+                           'aspect_ratio': q.aspect_ratio,
+                           'score': q.score,
+                           'length': q.length,
+                           'imdb_id': q.imdb_id,
+                           'review_data_dict': {}}
 
-                genre_str += str(genre)
-
-            occupation_dict = {}
+            # Add people to return dictionary
             for name_occupation_key in q.name_occupation_key_list:
-                occupation_obj = NameOccupation.get(name_occupation_key)
-                if occupation_obj.occupation not in occupation_dict:
-                    occupation_dict[str(occupation_obj.occupation)] = []
+                occupation_obj = models.NameOccupation.get(name_occupation_key)
+                if occupation_obj.occupation not in return_dict:
+                    return_dict[str(occupation_obj.occupation)] = []
 
-                occupation_dict[str(occupation_obj.occupation)].append(str(occupation_obj.name))
+                return_dict[str(occupation_obj.occupation)].append(str(occupation_obj.name))
 
-            review_obj_list = Review.all().ancestor(q)
-
-            self.response.write('<table>')
-            self.response.write('<tr><td></td><td><img src="%s"></td></tr>' % q.poster_url)
-            self.response.write('<tr><td>Title: </td><td>%s</td></tr>' % q.title)
-            self.response.write('<tr><td>Year: </td><td>%s</td></tr>' % q.year)
-            self.response.write('<tr><td>Plot: </td><td>%s</td></tr>' % q.plot)
-            self.response.write('<tr><td>Tagline: </td><td>%s</td></tr>' % q.tagline) 
-            if q.budget:
-                self.response.write('<tr><td>Budget: </td><td>%s</td></tr>' % q.budget)
-
-            if q.gross:
-                self.response.write('<tr><td>Gross: </td><td>%s</td></tr>' % q.gross)
-
-            self.response.write('<tr><td>Rating: </td><td>%s</td></tr>' % q.rating)
-            self.response.write('<tr><td>Video Type: </td><td>%s</td></tr>' % q.video_type)
-            self.response.write('<tr><td>Genres: </td><td>%s</td></tr>' % genre_str)
-            self.response.write('<tr><td>Aspect Ratio: </td><td>%s</td></tr>' % q.aspect_ratio)
-            self.response.write('<tr><td>Score: </td><td><strong>%s</strong></td></tr>' % q.score)
-            self.response.write('<tr><td>Length: </td><td>%s</td></tr>' % q.length)
-            self.response.write('<tr><td>IMDB ID: </td><td>%s</td></tr>' % q.imdb_id)
-
-            for (occupation, person_list) in occupation_dict.iteritems():
-                self.response.write('<tr><td><br /></td><td></td></tr>')
-                self.response.write('<tr><td>' + occupation + '(s):</td><td>')
-                for person in person_list:
-                    self.response.write(person + '<br />')
-
+            # Add reviews to return dictionary
+            review_obj_list = models.Review.all().ancestor(q)
             for review_obj in review_obj_list:
-                self.response.write('<tr><td><br /></td><td></td></tr>')
-                self.response.write('<tr><td>%s: </td><td><strong>%s</strong></td></tr>' % (review_obj.review_source, 
-                                                                                          review_obj.review_score))
-                if review_obj.review_author:
-                    self.response.write('<tr><td></td><td>%s</td></tr>' % review_obj.review_author)
-                if review_obj.review_content:
-                    self.response.write('<tr><td></td><td>%s</td></tr>' % review_obj.review_content)
-                if review_obj.review_date:
-                    self.response.write('<tr><td></td><td>Review Date: %s</td></tr>' % review_obj.review_date)
+                this_review_dict = {'review_score': review_obj.review_score,
+                                    'review_author': review_obj.review_author,
+                                    'review_content': review_obj.review_content,
+                                    'review_date': str(review_obj.review_date)}
 
-            self.response.write('</table><br /><br /><br /><br /><br />')
+                return_dict['review_data_dict'][review_obj.review_source] = this_review_dict
 
-        self.response.write('</body></html>')
-
+            self.response.write(json.dumps(return_dict, sort_keys=True, indent=4, separators=(',', ': ')))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
